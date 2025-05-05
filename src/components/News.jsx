7e0b46ce8571cb2874a120ba3f5500560b8360ed
Wilfred Manyara <manyarawilly46@gmail.com>
2025-05-05 09:23:24 +0300

    feat(infobyte): project setup

---
+++ src/components/News.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Weather from './Weather'
+import Calendar from './Calendar'
+import './News.css'
+
+const News = () => {
+  return (
+    <div className='news'>
+      <header className="news-header">News Header</header>
+      <div className="news-content">
+        <div className="navbar">
+          <div className="user">User</div>
+          <nav className="categories">Categories</nav>
+        </div>
+        <div className="news-section">
+          <div className="headline">Headline</div>
+          <div className="news-grid">News Grid</div>
+        </div>
+        <div className="my-blogs">My Blogs</div>
+        <Weather />
+        <Calendar />
+      </div>
+      <footer className="news-footer">Footer</footer>
+    </div>
+  )
+}
+
+export default News
