74af52cc3e6d91319c2ed15c0abad1a13e22f9ed
Wilfred Manyara <manyarawilly46@gmail.com>
2025-05-05 11:20:15 +0300

    feat(infobyte): add Navbar

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Weather from './Weather'
 import Calendar from './Calendar'
 import './News.css'
+import userImg from '../assets/images/user.jpg'
 
 const News = () => {
   return (
@@ -19,8 +20,45 @@ const News = () => {
       </header>
       <div className="news-content">
         <div className="navbar">
-          <div className="user">User</div>
-          <nav className="categories">Categories</nav>
+          <div className="user">
+            <img src={userImg} alt="User Image" />
+            <p>Wilfred's Blog</p>
+          </div>
+          <nav className="categories">
+            <h1 className="nav-heading">Categories</h1>
+            <div className="nav-links">
+              <a href="" className="nav-link">
+                General
+              </a>
+              <a href="" className="nav-link">
+                World
+              </a>
+              <a href="" className="nav-link">
+                Business
+              </a>
+              <a href="" className="nav-link">
+                Technology
+              </a>
+              <a href="" className="nav-link">
+                Entertainment
+              </a>
+              <a href="" className="nav-link">
+                Sports
+              </a>
+              <a href="" className="nav-link">
+                Science
+              </a>
+              <a href="" className="nav-link">
+                Health
+              </a>
+              <a href="" className="nav-link">
+                Nation
+              </a>
+              <a href="" className="nav-link">
+                Bookmarks <i className="fa-regular fa-bookmark"></i>
+              </a>
+            </div>
+          </nav>
         </div>
         <div className="news-section">
           <div className="headline">Headline</div>
