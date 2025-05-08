1758a2947127cc6752ed77aec83f9a7adf8a613c
Wilfred Manyara <manyarawilly46@gmail.com>
2025-05-08 10:46:52 +0300

    feat(infobyte): add weather functionality

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,111 @@
-import React from 'react'
-import './Weather.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Weather.css";
 
 const Weather = () => {
-  return (
-    <div className='weather'>Weather</div>
-  )
-}
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-export default Weather
+  useEffect(() => {
+    const fetchDefaultLocation = async () => {
+      const defaultLocation = "Nairobi";
+      const weatherKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=${weatherKey}`;
+
+      const response = await axios.get(url);
+      setData(response.data);
+    };
+    fetchDefaultLocation();
+  }, []);
+
+  const search = async () => {
+    const weatherKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${weatherKey}`;
+
+    try {
+      const response = await axios.get(url);
+      if (response.data.cod !== 200) {
+        setData({ notFound: true });
+      } else {
+        setData(response.data);
+        setLocation("");
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        setData({ notFound: true });
+      } else {
+        console.error("An unexpected error occurred", error);
+      }
+    }
+
+    console.log(response.data);
+  };
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
+
+  const getWeatherIcon = (weatherType) => {
+    switch (weatherType) {
+      case "Clear":
+        return <i className="bx bxs-sun"></i>;
+      case "Clouds":
+        return <i className="bx bxs-cloud"></i>;
+      case "Rain":
+        return <i className="bx bxs-cloud-rain"></i>;
+      case "Thunderstorm":
+        return <i className="bx bxs-cloud-lightning"></i>;
+      case "Snow":
+        return <i className="bx bxs-cloud-snow"></i>;
+      case "Haze":
+      case "Mist":
+        return <i className="bx bxs-cloud"></i>;
+      default:
+        return <i className="bx bxs-cloud"></i>;
+    }
+  };
+
+  return (
+    <div className="weather">
+      <div className="search">
+        <div className="search-top">
+          <i className="fa-solid fa-location-dot"></i>
+          <div className="location">{data.name}</div>
+        </div>
+        <div className="search-location">
+          <input
+            type="text"
+            placeholder="Enter Location"
+            value={location}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
+          <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
+        </div>
+      </div>
+      {data.notFound ? (
+        <div className="not-found">Not found😒</div>
+      ) : (
+        <div className="weather-data">
+          {data.weather &&
+            data.weather[0] &&
+            getWeatherIcon(data.weather[0].main)}
+          <div className="weather-type">
+            {data.weather ? data.weather[0].main : null}
+          </div>
+          <div className="temp">
+            {data.main ? `${Math.floor(data.main.temp)}°` : null}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
